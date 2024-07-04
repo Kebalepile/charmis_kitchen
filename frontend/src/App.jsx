ab2f709875f13236d721b3b4837e1b8cc1c74076
Kebalepile <pimpabutterfly86@gmail.com>
2024-07-04 20:35:15 +0200

    add 404 component with relevant styling & logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import NotFound from './components/NotFound'
 import './App.css'
 
 function App () {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <About />
-      <Contact />
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              <Navbar />
+              <Home />
+              <About />
+              <Contact />
+            </>
+          }
+        />
+        <Route path='/404' element={<NotFound />} />
+        <Route path='*' element={<Navigate to='/404' replace />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { TbError404 } from 'react-icons/tb'
+function NotFound () {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    // Redirect to home after 3 seconds
+    const timer = setTimeout(() => {
+      navigate('/')
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [navigate])
+
+  return (
+    <div className='not-found'>
+      <TbError404 className='img-404' />
+      <h1>tlhogela go kgotlakgotla!</h1>
+      <p>Redirecting to home page...</p>
+    </div>
+  )
+}
+
+export default NotFound
