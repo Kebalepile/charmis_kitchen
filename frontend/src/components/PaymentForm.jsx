290a0362078c2ba65aee8fcca729e57e83015d85
Kebalepile <pimpabutterfly86@gmail.com>
2024-07-09 12:42:05 +0200

    update payment form styling

--- frontend/src/components/PaymentForm.jsx
+++ frontend/src/components/PaymentForm.jsx
@@ -1,11 +1,9 @@
-import React, { useContext, useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import PaymentContext from '../context/payment/context'
-import Popup from './Popup'
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import PaymentContext from '../context/payment/context';
+import Popup from './Popup';
 
-const PaymentForm = ({ setShowPaymentForm, paymentItems,
-  resetOrderState
- }) => {
+const PaymentForm = ({ setShowPaymentForm, paymentItems, resetOrderState }) => {
   const {
     name,
     phone,
@@ -21,120 +19,123 @@ const PaymentForm = ({ setShowPaymentForm, paymentItems,
     handleHouseNumbersChange,
     handleStreetAddressChange,
     handleSubmitOrder,
-    resetPaymentState
-  } = useContext(PaymentContext)
+    resetPaymentState,
+  } = useContext(PaymentContext);
 
   useEffect(() => {
-    handlePaymentItems(paymentItems)
-  }, [paymentItems])
+    handlePaymentItems(paymentItems);
+  }, [paymentItems]);
 
-  const [showPopup, setShowPopup] = useState(false)
-  const [popupMessage, setPopupMessage] = useState('')
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupMessage, setPopupMessage] = useState('');
 
   const validateForm = () => {
-    const phoneRegex = /^0[0-9]{9}$/
-    const nameRegex = /^[a-zA-Z\s]+$/
+    const phoneRegex = /^0[0-9]{9}$/;
+    const nameRegex = /^[a-zA-Z\s]+$/;
 
     if (!phone.match(phoneRegex)) {
-      setPopupMessage('📞 Phone number must be 10 digits and start with 0.')
-      setShowPopup(true)
-      return false
+      setPopupMessage('📞 Phone number must be 10 digits and start with 0.');
+      setShowPopup(true);
+      return false;
     }
 
     if (!name.trim() || !name.match(nameRegex)) {
       setPopupMessage(
         '👤 Name must not be empty, contain only spaces, and should only include word characters.'
-      )
-      setShowPopup(true)
-      return false
+      );
+      setShowPopup(true);
+      return false;
     }
 
     // Additional validation logic as needed
 
-    return true
-  }
+    return true;
+  };
 
-  const handleFormSubmit = e => {
-    e.preventDefault()
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
     if (validateForm()) {
-      
-      handleSubmitOrder()
-      resetOrderState()
-      resetPaymentState()
-      setShowPaymentForm(false)
+      handleSubmitOrder();
+      resetOrderState();
+      resetPaymentState();
+      setShowPaymentForm(false);
       // Reset form if needed
     }
-  }
+  };
 
   const closePopup = () => {
-    setShowPopup(false)
-    setPopupMessage('')
-  }
+    setShowPopup(false);
+    setPopupMessage('');
+  };
 
-  const handleCloseFrom = () => {
-    // console.log('close payment form')
-    resetPaymentState()
-    setShowPaymentForm(false)
-  }
+  const handleCloseForm = () => {
+    resetPaymentState();
+    setShowPaymentForm(false);
+  };
+
   const renderAddressInputs = () => {
     if (paymentMethod === 'self-collect') {
-      return null // Render nothing if self-collect
+      return null; // Render nothing if self-collect
     }
 
     return (
       <div>
         <div className='form-group'>
           <label>
-            Street Address:
+            {/* Street Address: */}
             <input
               type='text'
               value={streetAddress}
               onChange={handleStreetAddressChange}
+              placeholder='Street Address'
               required
             />
           </label>
         </div>
         <div className='form-group'>
           <label>
-            House Number:
+            {/* House Number: */}
             <input
               type='text'
               value={houseNumber}
               onChange={handleHouseNumbersChange}
+              placeholder='House Number'
               required
             />
           </label>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div id='payment-form'>
-      <div className='overlay show' onClick={handleCloseFrom}></div>
+      <div className='overlay show' onClick={handleCloseForm}></div>
       <div className='payment-details'>
         <form onSubmit={handleFormSubmit}>
           <div className='form-group'>
             <h3>Payment details</h3>
-            <hr className='bg-hr' />
+            <hr  />
             <label>
-              Name:
+              {/* Name: */}
               <input
                 type='text'
                 value={name}
                 onChange={handleNameChange}
+                placeholder='Name'
                 required
               />
             </label>
           </div>
           <div className='form-group'>
             <label>
-              Phone (RSA only):
+              {/* Phone (RSA only): */}
               <input
                 type='tel'
                 value={phone}
                 onChange={handlePhoneChange}
                 pattern='[0-9]{10}'
+                placeholder='Phone number'
                 required
               />
             </label>
@@ -145,27 +146,21 @@ const PaymentForm = ({ setShowPaymentForm, paymentItems,
               <select
                 className='select-payment'
                 value={paymentMethod}
-                onChange={e => {
-                  console.log("method: ", e.target.value)
+                onChange={(e) => {
                   switch (e.target.value.trim()) {
                     case 'online':
                     case 'cash':
                       setPopupMessage(
                         '🚚 Delivery is currently limited to 📍 Boitekong Ext 2, 4, 5, 6, and 8'
-                      )
-                      
-                      setShowPopup(true)
-                      break
+                      );
+                      setShowPopup(true);
+                      break;
                   }
-                  handlePaymentChange(e)
+                  handlePaymentChange(e);
                 }}
               >
                 <option value='self-collect'>Self Collect (Free)</option>
                 <option value='cash'>Cash on Delivery (+R10.00)</option>
-                {/* <option value='online-self-collect'>
-                  Online Payment + Self Collect (Free)
-                </option>
-                <option value='online'>Online Payment (+R15.00)</option> */}
               </select>
             </label>
           </div>
@@ -183,19 +178,20 @@ const PaymentForm = ({ setShowPaymentForm, paymentItems,
             Pay
           </button>
 
-          <button type='button' className='cancel' onClick={handleCloseFrom}>
+          <button type='button' className='cancel' onClick={handleCloseForm}>
             Cancel
           </button>
         </form>
       </div>
       {showPopup && <Popup message={popupMessage} onClose={closePopup} />}
     </div>
-  )
-}
+  );
+};
 
 PaymentForm.propTypes = {
   setShowPaymentForm: PropTypes.func.isRequired,
   paymentItems: PropTypes.array.isRequired,
-  resetOrderState:PropTypes.func.isRequired
-}
-export default PaymentForm
+  resetOrderState: PropTypes.func.isRequired,
+};
+
+export default PaymentForm;
