f91d2feb8a45dd778d953346f4f2c879f43e30e3
Kebalepile <pimpabutterfly86@gmail.com>
2025-01-06 13:47:43 +0200

    2025 updates

--- frontend/boitekong_eats/src/App.jsx
+++ frontend/boitekong_eats/src/App.jsx
@@ -66,7 +66,6 @@ function App() {
           path='/'
           element={
             <>
-             {/* <HolidayShutdown /> */}
               <Navbar />
               <Home />
               <About />
